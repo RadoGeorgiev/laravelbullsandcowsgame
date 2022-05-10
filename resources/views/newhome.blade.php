bcb98179805375e64a3c785bd95a7bbeb67c0cb3
RadoGeorgiev <45625675+RadoGeorgiev@users.noreply.github.com>
2022-05-10 23:47:38 +0300

    Added project files

---
+++ resources/views/newhome.blade.php
@@ -0,0 +1,11 @@
+@extends('master')
+
+@section('title', 'Game')
+
+@section('content')
+
+	<h1 class="center">Welcome to Bulls And Cows Game!</h1>
+	
+	<div><img src="/svg/icon.svg" style="height: 25px;"></div>
+
+@endsection
